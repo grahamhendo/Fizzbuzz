dadf912a4331a42e987836ec8453141de590ab43
grahamhendo <grahamhendo@gmail.com>
2019-09-02 14:06:12 +0100

    Created fizzbuzz script

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,6 +2,19 @@
 """
 Spyder Editor
 
-This is a temporary script file.
+This code lists the numbers from 1 to 100.  If the number is divisible by 3
+fizz is printed, if the number is divisible by 5 then buzz is printed.  If the
+number is divisible by 3 and 5 then fizzbuzz is printed.
 """
 
+for x in range(1, 100):
+    fizz = x % 3
+    buzz = x % 5
+    if fizz == 0 and buzz == 0:
+        print("fizzbuzz") 
+    elif fizz == 0:
+        print("fizz") 
+    elif buzz == 0:  
+        print("buzz") 
+    else: print(x)
+    
