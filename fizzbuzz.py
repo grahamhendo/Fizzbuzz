5296f67b27726fcadd02c2ae6207c9a3eb3cad49
grahamhendo <grahamhendo@gmail.com>
2019-09-02 14:07:54 +0100

    Changed 100 to 101

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -7,7 +7,7 @@ fizz is printed, if the number is divisible by 5 then buzz is printed.  If the
 number is divisible by 3 and 5 then fizzbuzz is printed.
 """
 
-for x in range(1, 100):
+for x in range(1, 101):
     fizz = x % 3
     buzz = x % 5
     if fizz == 0 and buzz == 0:
